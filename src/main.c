f4ff5c7dbd2912ea269c4ee98020f7742280bfd6
Chip <chipz68q@gmail.com>
2018-12-15 19:10:57 +0500

    Add button load image handler
    Load image to "image" widget, save image
    filename to entry

--- src/main.c
+++ src/main.c
@@ -2,15 +2,18 @@
 
 #define	SPACING	5
 
+static void on_button_load_image_clicked(GtkWidget *button, gpointer data);
+
+static GtkWidget	*window;
+static GtkWidget	*image;
+static GtkWidget	*entry_image_filename;
+
 int main(int argc, char *argv[])
 {
-	GtkWidget	*window;
 	GtkWidget	*vbox;
 	GtkWidget	*hbox;
 	GtkWidget	*frame_image;
-	GtkWidget	*image;
 	GtkWidget	*button_load_image;
-	GtkWidget	*entry_image_filename;
 
 	gtk_init(&argc, &argv);
 
@@ -30,8 +33,34 @@ int main(int argc, char *argv[])
 	gtk_box_pack_start(GTK_BOX(hbox), frame_image, TRUE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(hbox), vbox, FALSE, FALSE, 0);
 	gtk_container_add(GTK_CONTAINER(window), hbox);
+	g_signal_connect(G_OBJECT(button_load_image), "clicked",
+		G_CALLBACK(on_button_load_image_clicked), NULL);
 	g_signal_connect(G_OBJECT(window), "destroy",
 		G_CALLBACK(gtk_main_quit), NULL);
 	gtk_widget_show_all(window);
 	gtk_main();
+}
+
+static void on_button_load_image_clicked(GtkWidget *button, gpointer data)
+{
+	GtkWidget	*dialog;
+	gchar		*filename;
+
+	dialog = gtk_file_chooser_dialog_new(NULL, GTK_WINDOW(window),
+		GTK_FILE_CHOOSER_ACTION_OPEN,
+		"Cancel", GTK_RESPONSE_CANCEL,
+		"Open", GTK_RESPONSE_ACCEPT,
+		NULL);
+	gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dialog),
+		g_get_home_dir());
+
+	if (gtk_dialog_run(GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
+	{
+		filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+		gtk_entry_set_text(GTK_ENTRY(entry_image_filename), filename);
+		gtk_image_set_from_file(GTK_IMAGE(image), filename);
+		g_free(filename);
+	}
+
+	gtk_widget_destroy (dialog);
 }
